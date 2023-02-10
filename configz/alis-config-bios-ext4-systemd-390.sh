5768e477f68a5bb394d4f830b8e0732696c03a66
Erik Dubois <erik.dubois@gmail.com>
2023-02-10 11:12:34 +0100

    390 dkms

---
+++ configz/alis-config-bios-ext4-systemd-390.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# laptop with nvidia 390 dkms
+set -eu
+
+sed -i "s/SWAP_SIZE=.*/SWAP_SIZE=\"16384\"/" /alis/alis.conf
+sed -i "s/DISPLAY_DRIVER=.*/DISPLAY_DRIVER=\"nvidia-390xx-dkms\"/" /alis/alis.conf
