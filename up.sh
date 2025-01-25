8a8418673f9a76de6de025ed523e5a4075c76506
Erik Dubois <erik.dubois@gmail.com>
2025-01-25 14:54:49 +0100

    update

--- up.sh
+++ up.sh
@@ -40,8 +40,18 @@ sed -i "s/\(^ISO_BUILD=\).*/\1$date_build/" files/etc/dev-rel
 # Folder to check
 FOLDER="output-archlinux-alis-virtualbox"
 
-# Set default alis.conf back
-cp alis.bconf alis.conf
+# Prompt the user
+read -p "Do you want to set the default alis.conf back? (y/n): " response
+
+# Check the response
+if [[ "$response" == "y" || "$response" == "Y" ]]; then
+  # Set default alis.conf back
+  cp alis.bconf alis.conf
+  echo "Default alis.conf has been restored."
+else
+  # Do nothing
+  echo "No changes made."
+fi
 
 # Check if the folder exists
 if [ -d "$FOLDER" ]; then
